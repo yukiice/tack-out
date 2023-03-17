69467f16845766f67b2cb506997c08b73db30c27
yukiice <58852690+yukiice@users.noreply.github.com>
2023-03-17 11:14:04 +0800

    初始化后端项目

---
+++ take-out-server/src/test/java/com/yukiice/takeoutserver/TakeOutServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukiice.takeoutserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeOutServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
