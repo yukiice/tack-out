aa039ff071eadb324f2e78f0bc28210e2bc57cca
yukiice <58852690+yukiice@users.noreply.github.com>
2023-11-10 10:43:08 +0800

    备份

---
+++ take-out-server/src/main/java/com/yukiice/controller/TokenController.java
@@ -0,0 +1,16 @@
+package com.yukiice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yukiice
+ * @version 1.0
+ * Create by 2023/9/30 11:58
+ */
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+}
+
--- take-out-server/src/test/java/com/yukiice/takeoutserver/TakeOutServerApplicationTests.java
+++ take-out-server/src/test/java/com/yukiice/takeoutserver/TakeOutServerApplicationTests.java
@@ -3,11 +3,46 @@ package com.yukiice.takeoutserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.security.MessageDigest;
+
 @SpringBootTest
 class TakeOutServerApplicationTests {
 
-    @Test
-    void contextLoads() {
+
+    public static final char[] chs = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    /**
+     * MD5加密
+     * @param str
+     * @return 32位16进制字符串,无符号
+     * @throws Exception
+     */
+    public static String getMD5(String str) throws Exception {
+
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        md5.update(str.getBytes("UTF-8"));
+        byte[] bys = md5.digest();
+
+        StringBuilder sb = new StringBuilder(32);
+        int index;
+        for (byte b : bys) {
+            // 将Java 中带符号位的byte 转换为不带符号位的
+            index = b & 0xff;
+            // 16进制数的高位
+            sb.append(chs[index >> 4]);
+            // 16进制数的低位
+            sb.append(chs[index % 16]);
+        }
+        // 返回的结果为32位的16进制数字符串
+        return sb.toString();
     }
 
+    @Test
+    void contextLoads() throws Exception {
+        String appKey = "yunhetitian";
+        String appKeyPwd = "I7x5W2p7t5e3J4B7";
+        String time = String.valueOf(System.currentTimeMillis());
+        String signature = getMD5(getMD5(appKey+"_"+appKeyPwd)+time);
+        System.out.println(time);
+        System.out.println(signature);
+    }
 }
